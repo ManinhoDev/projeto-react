986278072a30a84259a1a656e4d9779ff17d84e7
ManinhoDev <cristovao96958dsi@gmail.com>
2025-05-13 15:03:25 -0300

    primeiro commit do projeto react

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <header>
+        <h1>Carlos Augusto</h1>
+        <p>Tec de informática , cusrsando FullStack DSI PMERJ-FIRJAN</p>
+      </header>
+      
+      <main>
+        <h2>Sobre mim:</h2>
+        <p>Lorem ipsum dolor sit amet <strong> consectetur</strong> adipisicing elit.
+           Eaque totam, in eos magni illum fugiat esse sunt, fuga laudantium tenetur vitae voluptate
+           assumenda inventore commodi eligendi voluptatum alias ut laboriosam!</p>
+
+        <p>Lorem ipsum dolor sit amet <strong> consectetur</strong> adipisicing elit.
+           Eaque totam, in eos magni illum fugiat esse sunt, fuga laudantium tenetur vitae voluptate
+           assumenda inventore commodi eligendi voluptatum alias ut laboriosam!</p>
+
+           <h3>Habilidades Principais</h3>
+           <ul>
+              <li>Montagem e configuração de Pcs </li>
+              <li>Redes e conectividades</li>
+              <li>Sistemas Operacoionais (Windows, Linux, MS-DOS)</li>
+              <li>Didática técnica para cursos e certificações</li>
+
+
+
+           </ul>
+           
+           
+
+
+
+      </main>
+
+<footer>
+  &copy; Carlos Augusto Cristovão da Silva - Todos Direitos Reservados.
+
+</footer>
+  
+    </>
+  )
+}
+
+export default App
